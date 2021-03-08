540afaacbb2921e73253af7faa3c6795fd056bd9
derekchancts-at-878661647005 <derekchancts@yahoo.com.hk>
2021-03-09 05:13:31 +0800

    refactor code for signin and signup components / added Reset Password Page

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -3,6 +3,7 @@ import './styles.scss';
 
 import {auth, handleUserProfile} from '../../firebase/utils';
 
+import AuthWrapper from '../AuthWrapper';
 import FormInput from '../forms/FormInput';
 import Button from '../forms/Button';
 
@@ -11,7 +12,7 @@ const initialState = {
   email: '',
   password: '',
   confirmPassword: '',
-  errors: []
+  errors: [],
 };
 
 class Signup extends Component {
@@ -34,89 +35,136 @@ class Signup extends Component {
   handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { displayName, email, password, confirmPassword, errors } = this.state;
-    
-    if(password !== confirmPassword) {
-      const err = ['Password Don\'t match'];
+    const {displayName, email, password, confirmPassword, errors} = this.state;
+
+    if (password !== confirmPassword) {
+      const err = ["Password Don't match"];
       this.setState({
-        errors: err
-      })
+        errors: err,
+      });
       return;
     }
 
     try {
-      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      const {user} = await auth.createUserWithEmailAndPassword(email, password);
 
-      await handleUserProfile(user, { displayName });
+      await handleUserProfile(user, {displayName});
 
       this.setState({
-        ...initialState
-      })
-
+        ...initialState,
+      });
     } catch (err) {
-    
       console.log(err);
     }
-
-  }
-
+  };
 
   render() {
-    const {displayName, email, password, confirmPassword, errors } = this.state;
+    const {displayName, email, password, confirmPassword, errors} = this.state;
+
+    const configAuthWrapper = {
+      headline: 'Signup'
+    };
+
 
     return (
-      <div className='signup'>
-        <div className='wrap'>
-          <h2>Signup</h2>
+      // <div className='signup'>
+      //   <div className='wrap'>
+      //     <h2>Signup</h2>
+
+      //     {errors.length > 0 && (
+      //       <ul>
+      //         {errors.map((err, index) => {
+      //           return (
+      //             <li key={index}>
+      //               {err}
+      //             </li>
+      //           )
+      //         })}
+      //       </ul>
+      //     )}
+
+      //     <div className='formWrap'>
+      //       <form onSubmit={this.handleSubmit}>
+      //         <FormInput
+      //           type='text'
+      //           name='displayName'
+      //           value={displayName}
+      //           placeholder='Full name'
+      //           onChange={this.handleChange}
+      //           // autocomplete="off"
+      //         />
+      //         <FormInput
+      //           type='email'
+      //           name='email'
+      //           value={email}
+      //           placeholder='Email'
+      //           onChange={this.handleChange}
+      //         />
+      //         <FormInput
+      //           type='password'
+      //           name='password'
+      //           value={password}
+      //           placeholder='Password'
+      //           onChange={this.handleChange}
+      //         />
+      //         <FormInput
+      //           type='password'
+      //           name='confirmPassword'
+      //           value={confirmPassword}
+      //           placeholder='Confirm Password'
+      //           onChange={this.handleChange}
+      //         />
+      //         <Button type='submit'>Register</Button>
+      //       </form>
+      //     </div>
+      //   </div>
+      // </div>
+
+      <AuthWrapper {...configAuthWrapper}>
+        <div className='formWrap'>
 
           {errors.length > 0 && (
             <ul>
               {errors.map((err, index) => {
-                return (
-                  <li key={index}>
-                    {err}
-                  </li>
-                )
+                return <li key={index}>{err}</li>;
               })}
             </ul>
           )}
 
-          <div className='formWrap'>
-            <form onSubmit={this.handleSubmit}>
-              <FormInput
-                type='text'
-                name='displayName'
-                value={displayName}
-                placeholder='Full name'
-                onChange={this.handleChange}
-                // autocomplete="off"
-              />
-              <FormInput
-                type='email'
-                name='email'
-                value={email}
-                placeholder='Email'
-                onChange={this.handleChange}
-              />
-              <FormInput
-                type='password'
-                name='password'
-                value={password}
-                placeholder='Password'
-                onChange={this.handleChange}
-              />
-              <FormInput
-                type='password'
-                name='confirmPassword'
-                value={confirmPassword}
-                placeholder='Confirm Password'
-                onChange={this.handleChange}
-              />
-              <Button type='submit'>Register</Button>
-            </form>
-          </div>
+          <form onSubmit={this.handleSubmit}>
+            <FormInput
+              type='text'
+              name='displayName'
+              value={displayName}
+              placeholder='Full name'
+              onChange={this.handleChange}
+              // autocomplete="off"
+            />
+            <FormInput
+              type='email'
+              name='email'
+              value={email}
+              placeholder='Email'
+              onChange={this.handleChange}
+            />
+            <FormInput
+              type='password'
+              name='password'
+              value={password}
+              placeholder='Password'
+              onChange={this.handleChange}
+            />
+            <FormInput
+              type='password'
+              name='confirmPassword'
+              value={confirmPassword}
+              placeholder='Confirm Password'
+              onChange={this.handleChange}
+            />
+            <Button type='submit'>Register</Button>
+          </form>
         </div>
-      </div>
+      </AuthWrapper>
     );
   }
 }
