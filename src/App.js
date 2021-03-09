e209e93fab187bc337b55738088ca3054387292e
derekchancts-at-878661647005 <derekchancts@yahoo.com.hk>
2021-03-09 17:02:03 +0800

    added Redux to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import { auth, handleUserProfile } from './firebase/utils';
 
+// IMPORT REDUX
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/User/user.actions';
+
 // LAYOUTS
 import MainLayout from './layouts/MainLayout';
 import HomepageLayout from './layouts/HomepageLayout';
@@ -13,38 +17,53 @@ import Login from './pages/Login';
 import Recovery from './pages/Recovery';
 import './default.scss';
 
-const initialState = {
-  currentUser: null
-};
+// const initialState = {
+//   currentUser: null
+// };
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...initialState
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     ...initialState
+  //   };
+  // }
 
   authListener = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.authListener = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await handleUserProfile(userAuth);
         userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data()
-            }
+          // this.setState({
+          //   currentUser: {
+          //     id: snapshot.id,
+          //     ...snapshot.data()
+          //   }
+          // })
+
+          // const user = {
+          //   id: snapshot.id,
+          //   ...snapshot.data()
+          // }
+          // setCurrentUser(user)
+
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data()
           })
+
         })
       };
 
-      this.setState({
-        ...initialState
-      });
-
+      // this.setState({
+      //   ...initialState
+      // });
+      setCurrentUser(userAuth);   // can pass userAuth object here. As by default, it will return 
+                                  // null if there's no user
     });
   }
 
@@ -53,7 +72,8 @@ class App extends Component {
   }
 
   render() {
-    const { currentUser } = this.state;
+    // const { currentUser } = this.state;
+    const { currentUser } = this.props;
 
     return (
       <div className='App'>
@@ -64,7 +84,8 @@ class App extends Component {
             exact
             path='/'
             render={() => !currentUser ? <Redirect to="/login" /> : (
-              <HomepageLayout currentUser={currentUser}>
+              // <HomepageLayout currentUser={currentUser}>
+              <HomepageLayout>
                 <Homepage />
               </HomepageLayout>
             )}
@@ -72,7 +93,8 @@ class App extends Component {
           <Route
             path='/registration'
             render={() => currentUser ? <Redirect to="/" /> : (
-              <MainLayout currentUser={currentUser}>
+              // <MainLayout currentUser={currentUser}>
+              <MainLayout>
                 <Registration />
               </MainLayout>
             )}
@@ -80,7 +102,7 @@ class App extends Component {
           <Route
             path='/login'
             render={() => currentUser ? <Redirect to="/" /> : (
-              <MainLayout currentUser={currentUser}>
+              <MainLayout>
                 <Login />
               </MainLayout>
             )}
@@ -100,4 +122,13 @@ class App extends Component {
   }
 }
 
-export default App;
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
