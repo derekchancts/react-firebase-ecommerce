73b83d56a9a7bcf14d5bdf2687cdb51feb709d38
derekchancts-at-878661647005 <derekchancts@yahoo.com.hk>
2021-03-11 23:19:10 +0800

    added react hooks / custom hook withAuth checking user status

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, {useEffect} from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
-import { auth, handleUserProfile } from './firebase/utils';
+import {auth, handleUserProfile} from './firebase/utils';
 
 // IMPORT REDUX
-import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/User/user.actions';
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/User/user.actions';
+
+// HOC
+import WithAuth from './hoc/withAuth';
 
 // LAYOUTS
 import MainLayout from './layouts/MainLayout';
@@ -15,120 +18,152 @@ import Homepage from './pages/Homepage';
 import Registration from './pages/Registration';
 import Login from './pages/Login';
 import Recovery from './pages/Recovery';
+import Dashboard from './pages/Dashboard'; 
 import './default.scss';
 
-// const initialState = {
-//   currentUser: null
-// };
+const App = (props) => {
+  // authListener = null;
+  const {setCurrentUser, currentUser} = props;
 
-class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     ...initialState
-  //   };
-  // }
+  useEffect(() => {
+    const authListener = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await handleUserProfile(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data(),
+          });
+        });
+      }
+      setCurrentUser(userAuth); // can pass userAuth object here. As by default, it will return
+      // null if there's no user
+    });
 
-  authListener = null;
+    return () => {
+      authListener(); // onAuthStateChanged returns a function that we can call to unsubsribe the auth event listener
+    };
+  }, []);
 
+  /*
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const {setCurrentUser} = props;
 
     this.authListener = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await handleUserProfile(userAuth);
-        userRef.onSnapshot(snapshot => {
-          // this.setState({
-          //   currentUser: {
-          //     id: snapshot.id,
-          //     ...snapshot.data()
-          //   }
-          // })
-
-          // const user = {
-          //   id: snapshot.id,
-          //   ...snapshot.data()
-          // }
-          // setCurrentUser(user)
-
+        userRef.onSnapshot((snapshot) => {
           setCurrentUser({
             id: snapshot.id,
-            ...snapshot.data()
-          })
-
-        })
-      };
-
-      // this.setState({
-      //   ...initialState
-      // });
-      setCurrentUser(userAuth);   // can pass userAuth object here. As by default, it will return 
-                                  // null if there's no user
+            ...snapshot.data(),
+          });
+        });
+      }
+      setCurrentUser(userAuth); // can pass userAuth object here. As by default, it will return
+      // null if there's no user
     });
   }
 
   componentWillUnmount() {
     this.authListener();
   }
+  */
 
-  render() {
-    // const { currentUser } = this.state;
-    const { currentUser } = this.props;
+  // const {currentUser} = props;
 
-    return (
-      <div className='App'>
-        <Switch>
-          {/* <Route exact path='/' component={Homepage} />
+  return (
+    <div className='App'>
+      <Switch>
+        {/* <Route exact path='/' component={Homepage} />
           <Route path='/registration' component={Registration} /> */}
-          <Route
-            exact
-            path='/'
-            render={() => !currentUser ? <Redirect to="/login" /> : (
+
+        {/* <Route
+          exact
+          path='/'
+          render={() =>
+            !currentUser ? (
+              <Redirect to='/login' />
+            ) : (
               // <HomepageLayout currentUser={currentUser}>
               <HomepageLayout>
                 <Homepage />
               </HomepageLayout>
-            )}
-          />
-          <Route
-            path='/registration'
-            render={() => currentUser ? <Redirect to="/" /> : (
+            )
+          }
+        />
+        <Route
+          path='/registration'
+          render={() =>
+            currentUser ? (
+              <Redirect to='/' />
+            ) : (
               // <MainLayout currentUser={currentUser}>
               <MainLayout>
                 <Registration />
               </MainLayout>
-            )}
-          />
-          <Route
-            path='/login'
-            render={() => currentUser ? <Redirect to="/" /> : (
+            )
+          }
+        />
+         */}
+
+        <Route
+          exact
+          path='/'
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
+        />
+
+        <Route
+          path='/registration'
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+
+        <Route
+          path='/login'
+          render={() => (
+            <MainLayout>
+              <Login />
+            </MainLayout>
+          )}
+        />
+        
+        <Route
+          path='/recovery'
+          render={() =>
+            <MainLayout>
+              <Recovery />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/dashboard'
+          render={() =>
+            <WithAuth>
               <MainLayout>
-                <Login />
+                <Dashboard />
               </MainLayout>
-            )}
-          />
-          <Route 
-            path='/recovery'
-            render={() => currentUser ? <Redirect to="/" /> : (
-              <MainLayout>
-                <Recovery />
-              </MainLayout>
-            )}
-          />
-        </Switch>
-      </div>
-    );
+            </WithAuth>
+          }
+        />
 
-  }
-}
+      </Switch>
+    </div>
+  );
+};
 
-
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
-})
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser,
+});
 
 const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Link}from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link, withRouter}from 'react-router-dom'
 
 import './styles.scss';
 import {auth, signInWithGoogle} from '../../firebase/utils';
@@ -8,47 +8,55 @@ import AuthWrapper from '../AuthWrapper';
 import FormInput from '../forms/FormInput';
 import Button from '../forms/Button';
 
-const initialState = {
-  email: '',
-  password: '',
-};
+// const initialState = {
+//   email: '',
+//   password: '',
+// };
 
-class SignIn extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...initialState,
-    };
-  }
+const SignIn = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
 
-  handleChange = (e) => {
-    const {name, value} = e.target;
-    this.setState({
-      [name]: value,
-    });
+  // const handleChange = (e) => {
+    // const {name, value} = e.target;
+    // this.setState({
+    //   [name]: value,
+    // });
     // this.setState({
     //   [e.target.name]: e.target.value
     // })
-  };
 
-  handleSubmit = async (e) => {
+  //   if (e.target.name === "email") {
+  //     setEmail(e.target.value)
+  //   }
+
+  //   if (e.target.name === "password") {
+  //     setPassword(e.target.value)
+  //   }
+  // };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const {email, password} = this.state;
+    // const {email, password} = this.state;
 
     try {
       await auth.signInWithEmailAndPassword(email, password);
       // const { user } = await auth.signInWithEmailAndPassword(email, password);
       // console.log(user)
-      this.setState({
-        ...initialState,
-      });
+      // this.setState({
+      //   ...initialState,
+      // });
+      setEmail('');
+      setPassword('');
+
+      props.history.push('/');
+
     } catch (error) {
       console.log(error);
     }
   };
 
-  render() {
-    const {email, password} = this.state;
+    // const {email, password} = this.state;
 
     const configAuthWrapper = {
       headline: 'Login'
@@ -95,20 +103,20 @@ class SignIn extends Component {
       // REFACTOR THE ABOVE BY USING AN AUTHWRAPPER
       <AuthWrapper {...configAuthWrapper}>
         <div className='formWrap'>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <FormInput
               type='email'
               name='email'
               value={email}
               placeholder='Email'
-              onChange={this.handleChange}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <FormInput
               type='password'
               name='password'
               value={password}
               placeholder='Password'
-              onChange={this.handleChange}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <Button type='submit'>Login</Button>
 
@@ -128,7 +136,7 @@ class SignIn extends Component {
         </div>
       </AuthWrapper>
     );
-  }
+  
 }
 
-export default SignIn;
+export default withRouter(SignIn);
